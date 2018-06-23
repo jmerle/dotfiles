f3b6f90e58743107dafedfcb2fc310d8f6f3d823
Jasper van Merle <jaspervmerle@gmail.com>
2018-06-23 22:39:56 +0200

    Aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -33,12 +33,12 @@ alias xdebug-disable='sudo phpdismod xdebug'
 
 # Web related
 alias apache-start='sudo systemctl start apache2 && apache-status'
-alias apache-stop='sudo systemctl stop apache2 && apache-status'
+alias apache-stop='sudo systemctl disable apache2 && apache-status'
 alias apache-restart='sudo systemctl restart apache2 && apache-status'
 alias apache-status='echo "Apache: $(sudo systemctl status apache2 | grep running &>/dev/null && echo "running" || echo "stopped")"'
 
 alias mysql-start='sudo systemctl start mysql && mysql-status'
-alias mysql-stop='sudo systemctl stop mysql && mysql-status'
+alias mysql-stop='sudo systemctl disable mysql && mysql-status'
 alias mysql-restart='sudo systemctl restart mysql && mysql-status'
 alias mysql-status='echo "MySQL: $(sudo systemctl status mysql | grep running &>/dev/null && echo "running" || echo "stopped")"'
 
