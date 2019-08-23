c491a0b50bce56a35c02a0f4af6c0049ef041b03
Jasper van Merle <jaspervmerle@gmail.com>
2019-08-23 22:56:11 +0200

    Add DevDocs aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -37,3 +37,10 @@ alias dc='docker-compose'
 # Ruby
 alias b='bundle'
 alias be='bundle exec'
+alias t='bundle exec thor'
+
+# DevDocs
+alias dd='cd ~/Projects/devdocs'
+alias ddsync='git fetch upstream && git merge upstream/master'
+alias ddclean='rm -rf public/docs/* && be thor docs:download --default'
+alias ddcheck='bundle exec thor updates:check --github-token $DEVDOCS_GH_TOKEN'
