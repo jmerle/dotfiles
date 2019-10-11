b7de36548acb9c02cf0789356ef963212ef16f13
Jasper van Merle <jaspervmerle@gmail.com>
2019-10-11 17:49:07 +0200

    Ruby 2.6.5

--- zsh/.zsh/zplug.zsh
+++ zsh/.zsh/zplug.zsh
@@ -38,10 +38,10 @@ fi
 
 # Install Ruby if it's not already installed
 if rbenv version | grep "system" &>/dev/null; then
-    echo "Installing Ruby 2.6.3 (this might take a while)"
+    echo "Installing Ruby 2.6.5 (this might take a while)"
     sudo apt install -y libssl-dev libreadline-dev zlib1g-dev
-    rbenv install 2.6.3
-    rbenv global 2.6.3
+    rbenv install 2.6.5
+    rbenv global 2.6.5
 fi
 
 # History substring search keybindings
