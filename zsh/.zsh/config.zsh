833a7add8e9de5f5ca50af3cb5b10287d67c3ea9
Jasper van Merle <jaspervmerle@gmail.com>
2020-04-15 19:24:41 +0200

    Set-up libsecret credential helper and fix keybindings

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -6,11 +6,3 @@ setopt LOCAL_OPTIONS
 setopt LOCAL_TRAPS
 setopt PROMPT_SUBST
 setopt IGNORE_EOF
-
-# Keybindings
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^?' backward-delete-char
